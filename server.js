b314789eb9854ac85f8fa6b96de0b168b7d702b2
Calvin <calvinshum1874@gmail.com>
2020-05-26 20:07:06 -0700

    Redirect to homepage when wrong url is input

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get(/.*/, (req, res) => {
+    res.redirect('/');
+})
 const port = process.env.PORT || 3000;
 app.listen(port);
